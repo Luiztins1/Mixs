abd4451394ff02538e48d288bac10e89ed574eb8
Luiz Gabriel de Almeida Martins Marmorecola <luizgabrielmartins2005@gmail.com>
2026-07-07 19:54:10 -0300

    feat: entity Album mapped.

--- src/main/java/com/github/Luiztins1/mixs/model/entity/Album.java
+++ src/main/java/com/github/Luiztins1/mixs/model/entity/Album.java
@@ -1,4 +1,43 @@
 package com.github.Luiztins1.mixs.model.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.util.Set;
+
+@Entity
+@Table(name = "album")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
 public class Album {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "release_date")
+    private LocalDate releaseDate;
+
+    @ManyToMany
+    @JoinTable(
+            name = "album_artist",
+            joinColumns = @JoinColumn(name = "album_id"),
+            inverseJoinColumns = @JoinColumn(name = "artist_id")
+    )
+    private Set<Artist> artists;
+
+    @Column(name = "country", nullable = false)
+    private String country;
+
+    @Column(name = "description", nullable = false)
+    private String description;
+
+
 }
