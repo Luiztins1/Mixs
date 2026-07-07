75b1fd0e556bd2945a466b581f3789e629244146
Luiz Gabriel de Almeida Martins Marmorecola <luizgabrielmartins2005@gmail.com>
2026-07-07 19:23:44 -0300

    init project.

---
+++ src/main/java/com/github/Luiztins1/mixs/MixsApplication.java
@@ -0,0 +1,13 @@
+package com.github.Luiztins1.mixs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MixsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MixsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/Luiztins1/mixs/MixsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.Luiztins1.mixs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MixsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
