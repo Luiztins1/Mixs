beacc7e088a6e927f8bbb5e42690edb9a3c1db1f
Luiz Gabriel de Almeida Martins Marmorecola <luizgabrielmartins2005@gmail.com>
2026-07-07 19:37:55 -0300

    feat: entity User mapped.

--- src/main/java/com/github/Luiztins1/mixs/model/entity/User.java
+++ src/main/java/com/github/Luiztins1/mixs/model/entity/User.java
@@ -1,4 +1,21 @@
 package com.github.Luiztins1.mixs.model.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "user")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
 }
