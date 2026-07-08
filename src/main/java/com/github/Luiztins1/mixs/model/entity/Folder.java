f599f9df639dc28ff25184fcb09d09531c6d1275
Luiz Gabriel de Almeida Martins Marmorecola <luizgabrielmartins2005@gmail.com>
2026-07-08 19:16:32 -0300

    feat: entity Folder mapped.

--- src/main/java/com/github/Luiztins1/mixs/model/entity/Folder.java
+++ src/main/java/com/github/Luiztins1/mixs/model/entity/Folder.java
@@ -1,4 +1,29 @@
 package com.github.Luiztins1.mixs.model.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+import java.util.Set;
+
+@Entity
+@Table(name = "folder")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
 public class Folder {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToMany
+    @JoinTable(
+            name = "folder_album",
+            joinColumns = @JoinColumn(name = "folder_id"),
+            inverseJoinColumns = @JoinColumn(name = "album_id")
+    )
+    private Set<Album> album;
 }
