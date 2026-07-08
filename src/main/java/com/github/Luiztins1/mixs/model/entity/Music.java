0ae247f6717424daf041ebeb378225beb00e75bc
Luiz Gabriel de Almeida Martins Marmorecola <luizgabrielmartins2005@gmail.com>
2026-07-08 19:09:46 -0300

    feat: entity Music mapped.

--- src/main/java/com/github/Luiztins1/mixs/model/entity/Music.java
+++ src/main/java/com/github/Luiztins1/mixs/model/entity/Music.java
@@ -1,4 +1,29 @@
 package com.github.Luiztins1.mixs.model.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "music")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
 public class Music {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "name_song", nullable = false)
+    private String nameSong;
+
+    @Column(name = "band", nullable = false)
+    private String band;
+
+    @Column(name = "release_date", nullable = false)
+    private LocalDate releaseDate;
 }
