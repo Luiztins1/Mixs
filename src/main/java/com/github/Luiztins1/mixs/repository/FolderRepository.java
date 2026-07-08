05aa50ef0775b193d614e5c61151f6d015a92ad5
Luiz Gabriel de Almeida Martins Marmorecola <luizgabrielmartins2005@gmail.com>
2026-07-08 19:20:31 -0300

    feat: created FolderRepository.

---
+++ src/main/java/com/github/Luiztins1/mixs/repository/FolderRepository.java
@@ -0,0 +1,7 @@
+package com.github.Luiztins1.mixs.repository;
+
+import com.github.Luiztins1.mixs.model.entity.Folder;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FolderRepository extends JpaRepository<Folder, Integer> {
+}
