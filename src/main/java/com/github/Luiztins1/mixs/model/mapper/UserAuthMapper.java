4b290a6f3303126414605370e5fa225618d5da4e
Luiz Gabriel de Almeida Martins Marmorecola <luizgabrielmartins2005@gmail.com>
2026-07-12 16:20:25 -0300

    feat: created UserAuthMapper.

---
+++ src/main/java/com/github/Luiztins1/mixs/model/mapper/UserAuthMapper.java
@@ -0,0 +1,31 @@
+package com.github.Luiztins1.mixs.model.mapper;
+
+import com.github.Luiztins1.mixs.controller.dto.UserAuthResponseDTO;
+import com.github.Luiztins1.mixs.model.entity.UserAuth;
+
+public class UserAuthMapper {
+
+    public static UserAuthResponseDTO toDto (UserAuth userAuth){
+        if(userAuth == null) return null;
+
+        return new UserAuthResponseDTO(
+                userAuth.getId(),
+                userAuth.getLogin(),
+                userAuth.getPassword(),
+                userAuth.getRoles()
+        );
+    }
+
+    public static UserAuth toEntity(UserAuthResponseDTO userAuthResponseDTO){
+        if(userAuthResponseDTO == null) return null;
+
+        UserAuth userAuth = new UserAuth();
+
+        userAuth.setId(userAuthResponseDTO.id());
+        userAuth.setLogin(userAuthResponseDTO.login());
+        userAuth.setPassword(userAuthResponseDTO.password());
+        userAuth.setRoles(userAuthResponseDTO.roles());
+
+        return userAuth;
+    }
+}
