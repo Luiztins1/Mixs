418a6e8dcdb99fff48d6fbd7339457640c9f4816
Luiz Gabriel de Almeida Martins Marmorecola <luizgabrielmartins2005@gmail.com>
2026-07-12 16:57:15 -0300

    feat: created DatabaseSeeder.

---
+++ src/main/java/com/github/Luiztins1/mixs/config/DatabaseSeeder.java
@@ -0,0 +1,29 @@
+package com.github.Luiztins1.mixs.config;
+
+import com.github.Luiztins1.mixs.model.entity.UserAuth;
+import com.github.Luiztins1.mixs.repository.UserAuthRepository;
+import com.github.Luiztins1.mixs.service.UserAuthService;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.List;
+
+@Configuration
+public class DatabaseSeeder {
+
+    @Bean
+    public CommandLineRunner commandLineRunner(UserAuthRepository userAuthRepository, PasswordEncoder passwordEncoder){
+        return args ->{
+            if(userAuthRepository.count() == 0){
+                var userAuth = new UserAuth();
+                userAuth.setLogin("manager");
+                userAuth.setPassword("manager123");
+                userAuth.setRoles(List.of("MANAGER"));
+                
+                userAuthRepository.save(userAuth);
+            }
+        };
+    }
+}
