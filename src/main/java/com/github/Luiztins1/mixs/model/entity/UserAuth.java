47ac899cf53cf11f138f78963a6c5d52b5981f99
Luiz Gabriel de Almeida Martins Marmorecola <luizgabrielmartins2005@gmail.com>
2026-07-12 16:05:59 -0300

    feat: created entity UserAuth and CustomAuthentication.

---
+++ src/main/java/com/github/Luiztins1/mixs/model/entity/UserAuth.java
@@ -0,0 +1,35 @@
+package com.github.Luiztins1.mixs.model.entity;
+
+import io.hypersistence.utils.hibernate.type.array.ListArrayType;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.Type;
+
+import java.io.Serializable;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Table(name = "user_auth")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class UserAuth implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id")
+    private UUID id;
+
+    @Column(name = "login", nullable = false, unique = true)
+    private String login;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Type(ListArrayType.class)
+    @Column(name = "roles", columnDefinition = "varchar[]")
+    private List<String> roles;
+}
