564dce24b14c4304aa13034dd29e16dbfa1854bc
Luiz Gabriel de Almeida Martins Marmorecola <luizgabrielmartins2005@gmail.com>
2026-07-12 16:12:21 -0300

    feat: created UserAuthResponseDTO.

---
+++ src/main/java/com/github/Luiztins1/mixs/controller/dto/UserAuthResponseDTO.java
@@ -0,0 +1,20 @@
+package com.github.Luiztins1.mixs.controller.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+
+import java.util.List;
+import java.util.UUID;
+
+public record UserAuthResponseDTO(
+        UUID id,
+
+        @NotBlank(message = "O login é obrigatório e não pode estar em branco.")
+        String login,
+
+        @NotBlank(message = "A senha é obrigatória e não pode estar em branco.")
+        String password,
+
+        @NotEmpty(message = "Adicione corretamente as roles.")
+        List<String> roles) {
+}
