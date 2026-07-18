59269b38bbc9c317fc1c2c6efd1c272f88d5b3ac
Luiz Gabriel de Almeida Martins Marmorecola <luizgabrielmartins2005@gmail.com>
2026-07-18 15:51:18 -0300

    feat: added two endpoits in AlbumController.

--- src/main/java/com/github/Luiztins1/mixs/controller/rest/AlbumController.java
+++ src/main/java/com/github/Luiztins1/mixs/controller/rest/AlbumController.java
@@ -1,14 +1,15 @@
 package com.github.Luiztins1.mixs.controller.rest;
 
+import com.github.Luiztins1.mixs.controller.dto.AlbumResponseDTO;
+import com.github.Luiztins1.mixs.model.mapper.AlbumMapper;
 import com.github.Luiztins1.mixs.service.AlbumService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RequestMapping("/api/v1/albums")
@@ -24,5 +25,30 @@ public class AlbumController {
         var search = albumService.searchNameAlbum(name);
 
         return ResponseEntity.ok(search);
+    }
+
+    @PostMapping
+    public ResponseEntity<AlbumResponseDTO> createdAlbum(@RequestBody AlbumResponseDTO albumResponseDTO){
+        var album = AlbumMapper.toEntity(albumResponseDTO);
+
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(album.getId())
+                .toUri();
+
+        return ResponseEntity.created(location).body(AlbumMapper.toDto(album));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<AlbumResponseDTO>> listAllAlbums(){
+        List<AlbumResponseDTO> albumResponseDTOList =
+                albumService.listAllAlbums()
+                        .stream()
+                        .map(AlbumMapper::toDto)
+                        .toList();
+
+        if(albumResponseDTOList.isEmpty()) return ResponseEntity.noContent().build();
+        return ResponseEntity.ok(albumResponseDTOList);
     }
 }
